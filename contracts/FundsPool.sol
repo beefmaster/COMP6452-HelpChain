2d189c8ce08c7d625cafe8892ac283324191fc95
Dylan Oldfield <dylan.oldfield@yahoo.com>
2021-07-28 18:51:44 +1000

    added receivers array to allow for iteration

--- contracts/FundsPool.sol
+++ contracts/FundsPool.sol
@@ -1,16 +1,18 @@
 pragma solidity >=0.7.0 <0.9.0;
+import "./ReceiverFactory.sol";
 
 
 contract FundsPool{
     address public admin;
     uint public funds;
+    ReceiverFactory receiverFactory;
     event ValueReceived(address user, uint amount);
 
     constructor(){
         admin = msg.sender;
     }
 
-    function distributeFunds(uint amount, address[] memory receivers) public onlyOwner{
+    function distributeFunds(uint amount) public onlyOwner{
         require(amount <= address(this).balance, "not enough funds");
         uint fundsToSend;
         fundsToSend = amount / receivers.length; 
