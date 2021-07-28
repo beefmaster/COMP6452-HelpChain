f4d69dbc982e5946931fa56fccec5a248430bc51
jamo95 <jamison.tsai@gmail.com>
2021-07-28 11:44:02 +0800

    giver first commit

---
+++ contracts/GiverFactory.sol
@@ -0,0 +1,59 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+// Contract that funds are received at
+// needs to contain an idenitifier
+contract Giver {
+
+    address public ownerGiver;
+    uint public funds;
+    event ValueGiven(address user, uint amount);
+
+    constructor(address ownerGiver_, address funds_) {
+        ownerGiver = ownerGiver_;
+        funds = funds_;
+    }
+
+    function addFunds(address ownerGiver_, uint amount) public {
+        funds += amount;
+        payable(ownerGiver_).transfer(amount);
+    }
+
+    function giveFunds(uint amount, address to_, address fundsPoolAddress_) public payable onlyOwner {
+        if (funds >= amount) {
+            funds -= amount;
+            Receiver r = new  Receiver(to_, fundsPoolAddress_);
+            r.receive({
+                from: address(this),
+                value: amount
+            });
+        }
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == ownerGiver);
+        _;
+    }
+}
+
+contract GiverFactory {
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    Giver[] givers;
+    function creategiver() public onlyOwner returns (address) {
+        Giver child = new Giver(address(this), 0);
+        givers.push(child);
+        return address(child);
+    }
+
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+}
